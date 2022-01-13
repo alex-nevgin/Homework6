7164d0a330f6919e168213cc243b1e6958257629
Alexander Nevgin <alexblackstar86@gmail.com>
2022-01-13 16:58:40 +0300

    Fix of behaviour

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -6,10 +6,10 @@ using System.Threading.Channels;
 /// Метод для добавления данных в файл
 /// </summary>
 /// <param name="s">Строка для добавления</param>
-void Append(string[] s)
+void Append(string s)
 {
-	string path = @"D:\Test\test.txt";
-	File.AppendAllLines(path, s);
+    string path = @"test.txt";
+    File.AppendAllText(path, s);
 }
 
 /// <summary>
@@ -17,13 +17,18 @@ void Append(string[] s)
 /// </summary>
 void View()
 {
-	string path = @"D:\Test\test.txt";
-	string[] lines = File.ReadAllLines(path);
-	foreach (string line in lines)
+    string path = @"test.txt";
+    string[] lines = File.ReadAllLines(path);
+    for (int i = 0; i < lines.Length; i++)
     {
-        Console.Write(line + "\n");
+        string[] line = lines[i].Split('#');
+        foreach (string s in line)
+        {
+            Console.Write(s + " ");
+        }
+        Console.WriteLine();
     }
-
+    Console.WriteLine("");
 }
 
 /// <summary>
@@ -31,37 +36,64 @@ void View()
 /// </summary>
 void Action(string action)
 {
-	switch (action)
-	{
-		case "1":
-			if (File.Exists(@"D:\Test\test.txt"))
-			{
-				View();
-			}
-			else
-			{
-				File.Create(@"D:\Test\test.txt");
-			}
-			break;
-		case "2":
-			Console.WriteLine("Введите информацию для добавления (в формате: №, Дата добавления, ФИО, Возраст, Рост, Дата Рождения, Место Рождения");
-			string text = Console.ReadLine() + "#";
-			string[] line = text.Split('#');
-			Append(line);
-			break;
+    switch (action)
+    {
+        case "1":
+            if (File.Exists(@"D:\Test\test.txt"))
+            {
+                View();
+            }
+            else
+            {
+                File.Create(@"D:\Test\test.txt");
+            }
+            break;
+        case "2":
+            Console.WriteLine("Введите информацию для добавления");
+            string[] hints =
+            {
+                "Номер записи",
+                "Дата добавления",
+                "ФИО",
+                "Возраст",
+                "Рост",
+                "Дата рождения",
+                "Место рождения"
+            };
+            string data = "";
+            for (int i = 0; i < hints.Length; i++)
+            {
+                Console.WriteLine(hints[i]);
+                if (i < hints.Length - 1)
+                    data += Console.ReadLine() + "#";
+                else
+                    data += Console.ReadLine() + "\n";
+            }
+            Append(data);
+            break;
+        default:
+            Console.WriteLine("Такой операции не предусмотрено");
+            break;
 
-		default:
-			Console.WriteLine("Такой операции не предусмотрено");
-			break;
-	}
+    }
+
 }
 
 
-Console.WriteLine("Введите действие (1 - вывод файла (если файла нет, он будет создан); 2 - добавить информацию в файл)");
+do
+{
+    Console.WriteLine("Введите действие (1 - вывод файл а (если файла нет, он будет создан); 2 - добавить информацию в файл, 0 - выход)");
+    string action = Console.ReadLine();
+    if (action == "0")
+    {
+        break;
+    }
+    Console.WriteLine();
+    Action(action);
+} while (true);
 
-string action = Console.ReadLine();
-Console.WriteLine();
-Action(action);
-	
+
+
+
 
 
